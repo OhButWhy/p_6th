d5bbec2f568f13498345e16c79fb2f089a5e45eb
Pin <nado.09.ru@gmail.com>
2026-05-21 00:54:32 +0700

    step1

--- gpu1.cpp
+++ gpu1.cpp
@@ -97,31 +97,57 @@ int main(int argc, char* argv[])
     int iter = 0;
     auto start = std::chrono::steady_clock::now();
 
-    #pragma acc data copy(local_grid[0:ny * nx]) create(local_newgrid[0:ny * nx])
+    bool src_is_grid = true;
+    bool result_is_grid = true;
+
+    #pragma acc data copyin(local_grid[0:ny * nx], local_newgrid[0:ny * nx])
     {
         for (;;) {
             double maxdiff = 0.0;
             iter++;
             if (iter > max_iter) break;
 
-            #pragma acc parallel loop collapse(2) gang vector present(local_grid, local_newgrid) reduction(max:maxdiff)
-            for (int i = 1; i < ny - 1; i++) {
-                for (int j = 1; j < nx - 1; j++) {
-                    int ind = i * nx + j;
-                    local_newgrid[ind] = (local_grid[ind - nx] + local_grid[ind + nx] +
-                                          local_grid[ind - 1] + local_grid[ind + 1]) * 0.25;
-                    double diff = local_grid[ind] - local_newgrid[ind];
-                    if (diff < 0) diff = -diff;
-                    if (diff > maxdiff) maxdiff = diff;
+            if (src_is_grid) {
+                #pragma acc parallel loop collapse(2) gang vector present(local_grid, local_newgrid) reduction(max:maxdiff) async(1)
+                for (int i = 1; i < ny - 1; i++) {
+                    for (int j = 1; j < nx - 1; j++) {
+                        int ind = i * nx + j;
+                        local_newgrid[ind] = (local_grid[ind - nx] + local_grid[ind + nx] +
+                                              local_grid[ind - 1] + local_grid[ind + 1]) * 0.25;
+                        double diff = local_grid[ind] - local_newgrid[ind];
+                        if (diff < 0) diff = -diff;
+                        if (diff > maxdiff) maxdiff = diff;
+                    }
                 }
+                result_is_grid = false;
+            } else {
+                #pragma acc parallel loop collapse(2) gang vector present(local_grid, local_newgrid) reduction(max:maxdiff) async(1)
+                for (int i = 1; i < ny - 1; i++) {
+                    for (int j = 1; j < nx - 1; j++) {
+                        int ind = i * nx + j;
+                        local_grid[ind] = (local_newgrid[ind - nx] + local_newgrid[ind + nx] +
+                                           local_newgrid[ind - 1] + local_newgrid[ind + 1]) * 0.25;
+                        double diff = local_newgrid[ind] - local_grid[ind];
+                        if (diff < 0) diff = -diff;
+                        if (diff > maxdiff) maxdiff = diff;
+                    }
+                }
+                result_is_grid = true;
             }
 
-            double* tmp = local_grid;
-            local_grid = local_newgrid;
-            local_newgrid = tmp;
+            #pragma acc wait(1)
 
             max_error = maxdiff;
             if (maxdiff < eps) break;
+            src_is_grid = !src_is_grid;
+        }
+
+        if (N == 10 || N == 13) {
+            if (result_is_grid) {
+                #pragma acc update self(local_grid[0:ny * nx])
+            } else {
+                #pragma acc update self(local_newgrid[0:ny * nx])
+            }
         }
     }
 
@@ -135,7 +161,7 @@ int main(int argc, char* argv[])
         for (int i = 0; i < nx; i++){
             for (int j = 0; j < ny; j++)
             {
-                std::cout<<local_grid[i * nx + j]<<' ';
+                std::cout<<(result_is_grid ? local_grid : local_newgrid)[i * nx + j]<<' ';
             }
             std::cout<<std::endl;
         }
