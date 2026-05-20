4a9c1d45eed8cb8c59a8218b5da16b14fed09ca0
Pin <nado.09.ru@gmail.com>
2026-05-21 00:14:48 +0700

    base0

--- gpu1.cpp
+++ gpu1.cpp
@@ -104,22 +104,24 @@ int main(int argc, char* argv[])
             iter++;
             if (iter > max_iter) break;
 
-            #pragma acc parallel loop gang vector_length(256) reduction(max:maxdiff)
+            #pragma acc parallel loop collapse(2) gang vector present(local_grid, local_newgrid) reduction(max:maxdiff)
             for (int i = 1; i < ny - 1; i++) {
-                #pragma acc loop reduction(max:maxdiff)
                 for (int j = 1; j < nx - 1; j++) {
                     int ind = i * nx + j;
                     local_newgrid[ind] = (local_grid[ind - nx] + local_grid[ind + nx] +
-                                        local_grid[ind - 1] + local_grid[ind + 1]) * 0.25;
+                                          local_grid[ind - 1] + local_grid[ind + 1]) * 0.25;
                     double diff = local_grid[ind] - local_newgrid[ind];
                     if (diff < 0) diff = -diff;
                     if (diff > maxdiff) maxdiff = diff;
                 }
             }
 
-            double* tmp = local_grid;
-            local_grid = local_newgrid;
-            local_newgrid = tmp;
+            #pragma acc parallel loop collapse(2) present(local_grid, local_newgrid)
+            for (int i = 0; i < ny; i++) {
+                for (int j = 0; j < nx; j++) {
+                    local_grid[i * nx + j] = local_newgrid[i * nx + j];
+                }
+            }
 
             max_error = maxdiff;
             if (maxdiff < eps) break;
